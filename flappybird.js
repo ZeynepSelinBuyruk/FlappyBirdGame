371cbba7fe6e56db2bab4effd4c2f9b5831b7acc
Zeynep Selin Buyruk <zbuyruk88@gmail.com>
2025-12-14 21:16:05 +0300

    Add Flappy Bird game assets and update HTML

---
+++ flappybird.js
@@ -0,0 +1,38 @@
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+//bird
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
+let birdImg;
+let bird = {
+    x: birdX,
+    y: birdY,
+    width: birdWidth,
+    height: birdHeight
+}
+
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.width = boardWidth;
+    board.height = boardHeight;
+    context = board.getContext("2d");
+
+    //draw bird
+    context.fillStyle = "skyblue";
+    context.fillRect(bird.x, bird.y, bird.width, bird.height);
+
+    //load image
+    birdImg = new Image();
+    birdImg.src = "./flappybird.png";
+    birdImg.onload = function() {
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+}   
+}
+
