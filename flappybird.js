41b64620cc5665c09b7b6031a69bb5a5d80ab050
Zeynep Selin Buyruk <zbuyruk88@gmail.com>
2026-01-01 18:21:49 +0300

    Add pipes and collision detection to Flappy Bird game

--- flappybird.js
+++ flappybird.js
@@ -22,6 +22,16 @@ let bird = {
 let velocityY = 0; //bird jump speed
 let gravity = 0.4;
 
+//pipes
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
 //game loop
 let gameRunning = false;
 
@@ -37,6 +47,12 @@ window.onload = function() {
     birdImg.onload = function() {
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
+
+    topPipeImg = new Image();
+    topPipeImg.src = "./toppipe.png";
+
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "./bottompipe.png";
 
     //start game
     gameRunning = true;
@@ -57,6 +73,32 @@ function update() {
     //draw bird
     context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
 
+    //pipes
+    for (let i = 0; i < pipeArray.length; i++) {
+        let pipe = pipeArray[i];
+        pipe.x += -2; //move left
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+        if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+            pipe.passed = true;
+        }
+
+        //collision
+        if (detectCollision(bird, pipe)) {
+            gameRunning = false;
+        }
+    }
+
+    //clear pipes
+    while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
+        pipeArray.shift();
+    }
+
+    //spawn pipes
+    if (pipeArray.length == 0 || pipeArray[pipeArray.length - 1].x < boardWidth - 200) {
+        createPipe();
+    }
+
     //check boundaries
     if (bird.y + bird.height >= boardHeight || bird.y <= 0) {
         gameRunning = false;
@@ -72,6 +114,39 @@ function jump() {
         requestAnimationFrame(update);
     }
     velocityY = -8; //jump up
+}
+
+//pipe functions
+function createPipe() {
+    let randomPipeY = pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2);
+    let openingSpace = boardHeight / 4;
+
+    let topPipe = {
+        img: topPipeImg,
+        x: pipeX,
+        y: randomPipeY,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    }
+    pipeArray.push(topPipe);
+
+    let bottomPipe = {
+        img: bottomPipeImg,
+        x: pipeX,
+        y: randomPipeY + pipeHeight + openingSpace,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    }
+    pipeArray.push(bottomPipe);
+}
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
 }
 
 //event listeners
